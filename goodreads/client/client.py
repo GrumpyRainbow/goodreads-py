b308c0ea0cce046aec461ab17985d14c3f952678
Brian Bridges <brian.brdgs@gmail.com>
2013-12-08 17:40:12 -0600

    Extract Request logic to separate module Add httpretty to requirements Add test_client_instance_is_created Add test_host_is_set Add test_book_title Add book_title_response.xml

--- goodreads/client/client.py
+++ goodreads/client/client.py
@@ -1,35 +1,8 @@
-import httplib2
-import urllib
-import xmltodict
 import datetime
 
 from author import Author
 from book import Book
-
-class GoodreadsRequestError(Exception):
-    def __init__(self, error_msg, url):
-        self.error_msg = error_msg
-        self.url = url
-
-    def __str__(self):
-        return self.error_msg + "\n" + self.url
-
-class GoodreadsRequest:
-    def __init__(self, path, additional_query_info, client_instance):
-        self.query_dict = dict(client_instance.query_dict.items() + additional_query_info.items())
-        self.host = client_instance.host
-        self.path = path
-        if len(additional_query_info) > 0:
-            self.path += '?'
-
-    def request(self):
-        h = httplib2.Http('.cache')
-        url_extension = self.path + urllib.urlencode(self.query_dict)
-        response = h.request(self.host + url_extension, "GET")
-        data_dict = xmltodict.parse(response[-1])
-        if data_dict.has_key('error'):
-            raise GoodreadsRequestError(data_dict['error'], url_extension)
-        return data_dict['GoodreadsResponse']
+from request import GoodreadsRequest, GoodreadsRequestError
 
 class Client:
     """A client for interacting with Goodreads resources."""
--- test/test_client.py
+++ test/test_client.py
@@ -1,10 +1,47 @@
 import goodreads
+import datetime
+import httpretty
+import urllib
 
-from nose.tools import eq_
+from nose.tools import eq_, raises
+
+def test_client_instance_is_created():
+    """Test to see if an instance of Client is created from goodreads.Client"""
+    client = goodreads.Client()
+    assert isinstance(client, goodreads.Client)
 
 def test_kwargs_parsing_valid():
     """Test that valid kwargs are stored as properties on the client."""
     client = goodreads.Client(client_id='foo', client_secret='foo')
-    assert isinstance(client, goodreads.Client)
     eq_('foo', client.client_id)
     eq_('foo', client.client_secret)
+
+def test_host_is_set():
+    """Test that verifies that the host is set properly."""
+    client = goodreads.Client()
+    eq_("https://www.goodreads.com/", client.host)
+
+@httpretty.activate
+def test_book_title():
+    """Test that verifies information about a book is done properly."""
+    client = goodreads.Client(client_id="123abc")
+
+    base_url = "https://www.goodreads.com/"
+    api_call = "book/title.xml?"
+    query_dict = { 'author' : 'Chuck Palahniuk', 'title' : 'Fight Club' }
+
+    url = base_url + api_call + urllib.urlencode(query_dict)
+
+    sample_response = open('test/fixtures/book_title_response.xml')
+    body = sample_response.read()
+
+    httpretty.register_uri(httpretty.GET, url, body=body, status=200)
+
+    book = client.book_title(author='Chuck Palahniuk', book='Fight Club')
+
+    eq_('Fight Club', book.title)
+    eq_('5759', book.id)
+    eq_(list, type(book.authors))
+    for author in book.authors:
+        eq_('2546', author.id)
+        eq_('Chuck Palahniuk', author.name)
